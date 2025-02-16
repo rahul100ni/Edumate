a7c56f6927c84ddbb7619563741640afff802d35
rahul100ni <khaufnaak.kalakaar@gmail.com>
2025-02-16 14:26:18 +0530

    Footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 transition-colors duration-300">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2024 EduMate. All rights reserved.</p>
+        <p>&copy; 2025 EduMate. All rights reserved.</p>
       </div>
     </footer>
   )
